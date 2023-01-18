e557c9abfed3d3b2ff56ae54c38207b19be53323
Hamzah Qudsi <hqudsi@datawire.io>
2023-01-18 11:42:21 -0500

    build: simplify buf workspace
    Simplify buf workspace into a single module
    
    Signed-off-by: Hamzah Qudsi <hqudsi@datawire.io>

--- tools/make/proto.mk
+++ tools/make/proto.mk
@@ -3,7 +3,3 @@
 .PHONY: protos
 protos: $(tools/buf) ## Compile all protobufs
 	$(tools/buf) generate
-
-.PHONY: proto-extension
-proto-extension: $(tools/buf) ## Compile internal/extension/proto/extension.proto
-	$(tools/buf) generate internal/extension
